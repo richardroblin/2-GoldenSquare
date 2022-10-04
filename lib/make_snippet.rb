bac3fb6e3cc86aad6b422a5ebe91b8b593f1c8b7
Richard Roblin <richard.roblin@outlook.com>
2022-10-04 16:15:18 +0100

    added ... to test and method

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,5 +1,11 @@
 def make_snippet(arg)
     a=arg.split(" ", 10)
     b= "#{a[0]} " + "#{a[1]} " + "#{a[2]} " + "#{a[3]} " + "#{a[4]} "
-    return b.rstrip!
+    c= "..."
+    d= if a.count > 5
+            return "#{b.rstrip!}" + "#{c}"
+            else "#{b.rstrip!}"
+        end
+
+    return d
 end
--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -7,9 +7,13 @@ RSpec.describe 'make_snippet' do
     end
 
     it 'returns the first 5 words' do
-    result = make_snippet('Hello how are you feeling today')
+    result = make_snippet('Hello how are you feeling')
     expect(result).to eq 'Hello how are you feeling'
     end
 
-    
+    it 'return ... if there are more than 5 words' do
+    result = make_snippet('Hello how are you feeling today')
+    expect(result).to eq 'Hello how are you feeling...'
+    end
+
 end
