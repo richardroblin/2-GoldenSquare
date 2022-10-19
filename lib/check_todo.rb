55a72c0c2f5d39a1e1c912eed118e3ae3de461fd
Richard Roblin <richard.roblin@outlook.com>
2022-10-19 17:03:22 +0100

    apply deletions

--- lib/check_todo.rb
+++
@@ -1,9 +0,0 @@
-def check_todo(text)
-    fail "No todo in string" if text.empty?
-    if text.include?("todo")
-        return true
-    else false
-    end
-
-
-end
--- spec/todo_list_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'todo_list'
-
-RSpec.describe TodoList do
-    context '...' do
-    end
-end
