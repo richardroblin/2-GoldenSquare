547a6feb0a740e57dc1d74bb189d4d119321d9b8
Richard Roblin <richard.roblin@outlook.com>
2023-02-09 17:18:29 +0000

    folder changes

--- Phase 2/lib/count_words.rb
+++
@@ -1,3 +0,0 @@
-def count_words(arg)
-    arg.split(" ").size
-end
--- Phase 2/lib/reading_time.rb
+++
@@ -1,9 +0,0 @@
-def read_text(text)
-    words = text.split(" ").size
-    wpm = (words/200.to_f).ceil
-    if words == 0 
-        return 0
-    else words > 0 
-        return wpm
-    end
-end
